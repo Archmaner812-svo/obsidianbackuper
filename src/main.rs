0857029259a08c52b194a82f1a357eb416d71737
Archmaner812 <konstantinmosharsky@yandex.ru>
2026-01-16 22:53:33 +0300

    1.0.1
    fixed a small part of shit code and aded  getting token and you username in telegram.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use powershell_script;
 use std::time::Duration;
-use std::{io, str};
+use std::{fs, io, str};
 use fs_extra::copy_items;
 use fs_extra::dir::{CopyOptions, copy, create};
 use std::thread;
@@ -7,27 +8,12 @@ use std::os;
 use fs_extra::dir;
 use std::process::Command;
 use std::path::{self, Path};
+use std::fs::OpenOptions;
+use std::io::Write;
+
 
 fn main() {
-    println!("812");
-    let obs1 = get_obsidian_folder();
-    let obs2 = get_obsidian_folder_name(&obs1);
     
-    
-    
-    let whiler = true;
-    while whiler == true {
-        copy_directory_to_tmp_dir(&obs1);
-
-                        
-        
-        let zip_check = ziping_of_copy();
-        if zip_check == true{
-            println!("done")
-        }
-        thread::sleep(Duration::from_hours(1));
-        
-    }
     
 
 }
@@ -44,19 +30,17 @@ fn get_obsidian_folder() -> String {
     println!("Путь получен: {}",obsidian_folder_path);
     obsidian_folder_path.trim().to_string()
 }
-fn copy_directory_to_tmp_dir(obsidian_folder_path: &str) {
-    let mut tmp_dir = String::new();
-    tmp_dir = String::from(r"C:\ts2");
-    let tmp_dir2 = tmp_dir.clone();
-    fs_extra::dir::create_all(tmp_dir,true)
+fn copy_dir_to_tmp_dir(obsidian_folder_path: &str) {
+    let tmp_dir = String::from(r"C:\ts2");
+    fs_extra::dir::create_all(&tmp_dir,true)
         .expect("Недостаточно прав");
     
-    println!("{}",tmp_dir2);
+    println!("{}",&tmp_dir);
     let mut options = CopyOptions::new();
     options.copy_inside = true;
     options.overwrite = true;
 
-    copy(obsidian_folder_path,  tmp_dir2, &options)
+    copy(obsidian_folder_path,  &tmp_dir, &options)
     .expect("ошбика копирования");
 
 
@@ -91,4 +75,37 @@ fn get_obsidian_folder_name(path_to_folder:&String) -> String{
 
     folder_name
     
+}
+
+
+
+
+
+
+fn get_and_save_user_id(){
+    let mut user_id = String::new();
+    io::stdin()
+        .read_line(&mut user_id)
+        .expect("неудалось получить ваш id");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true) // создаёт файл, если не существует
+        .open("obsbackupsetting.txt")
+        .expect("Не удалось открыть файл");
+    writeln!(file,"{user_id}").expect("Не удалось запсать user id");
+    println!("если вы знаете что ошибились то найдите файл obsbackupsetting.txt и запиште туда ваш айди в телеграмм в во вторую строку");
+    };
+        
+}
+
+fn get_token(){
+    println!("Напиши токен своего бота");
+    let mut token = String::new();
+    io::stdin()
+        .read_line(&mut token)
+        .expect("");
+    token = format!("$env:TELOXIDE_TOKEN={}>",token);
+    println!("{}",token)
+
 }
